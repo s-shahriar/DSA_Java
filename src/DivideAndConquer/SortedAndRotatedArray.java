d4cf1b616ab53458d5d26763a5d0b706de6eedaf
Sayed Shahriar <shahriar@biznovelty.com>
2025-02-06 10:43:02 +0600

    Sorted And Rotated Array

--- src/DivideAndConquer/SortedAndRotatedArray.java
+++ src/DivideAndConquer/SortedAndRotatedArray.java
@@ -3,39 +3,47 @@ package DivideAndConquer;
 import java.util.Arrays;
 
 public class SortedAndRotatedArray {
-    public static int search(int[] nums, int target, int si, int ei) {
-        int mid = si + (ei - si) / 2;
+
+    public static int search(int[] nums, int target) {
+        return searchHelper(nums, target, 0, nums.length-1);
+    }
+
+    public static int searchHelper(int[] nums, int target, int si, int ei){
+        //Base case
+        if(si > ei) {
+            return -1;
+        }
+
+        int mid = si + (ei - si ) / 2;
 
         if(nums[mid] == target) {
             return mid;
         }
 
-        // Suppose its falls on L1 line
-        if(nums[si] <= mid){
-            // Case a: L1's left side
-            if(nums[si] <= target && target < nums[mid]) {
-                return search(nums, target, si, mid);
+        // Suppose the mid falls in Line 1
+        if(nums[si] <= nums[mid]){
+            // Case a: falls in Line 1's left side
+            if(target >= nums[si] && target <= nums[mid]){
+                return searchHelper(nums, target, si, mid-1);
             } else {
-                // Case b: falls on L1's right to L2
-                return search(nums, target, mid + 1, ei);
+                // Case b: falls in Line 1's right + Line 2 area
+                return searchHelper(nums, target, mid+1, ei);
             }
         } else {
-            // Falls on L2 line
-            if(nums[mid] <= target && target <= nums[ei]) {
-                // Case c: Falls on L2's right side
-                return search(nums, target, si, mid + 1);
+            // mid falls in Line 2
+            // Case c: falls in Line 2's right side
+            if(target >= nums[mid] && target <= nums[ei]){
+                return searchHelper(nums, target, mid+1, ei);
             } else {
-                // Case d: falls on L2's right to L1
-                return search(nums, target, mid + 1, ei);
+                return searchHelper(nums, target, si, mid-1);
             }
         }
     }
 
-
     public static void main(String[] args) {
         int[] nums = {4,5,6,7,0,1,2};
         int target = 0;
-        int ans = search(nums, target, 0 , nums.length-1);
+        int ans = search(nums, target);
         System.out.println("Provided Array = " + Arrays.toString(nums));
         System.out.println("ans = " + ans);
     }
